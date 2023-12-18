7005427124f4097fac4fa59852b5e2c54d1f3dc4
VishnuRathore98 <rathorevishnu666@gmail.com>
2023-12-18 09:42:28 +0530

    Running tests parallely using threads also attaching tests to threads. Implementing custom test logging.

--- src/main/java/reporting/ExtentReportManager.java
+++ src/main/java/reporting/ExtentReportManager.java
@@ -1,6 +1,8 @@
 package reporting;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
@@ -35,4 +37,42 @@ public class ExtentReportManager {
         String reportName = "TestReport"+formattedTime+".html";
         return reportName;
     }
+
+//  Implementing test execution result details.
+    public static void logPassDetails(String log){
+        Setup.extentTest.get().pass(MarkupHelper.createLabel(log, ExtentColor.GREEN));
+    }
+    public static void logFailDetails(String log){
+        Setup.extentTest.get().fail(MarkupHelper.createLabel(log, ExtentColor.RED));
+    }
+    public static void logInfoDetails(String log){
+        Setup.extentTest.get().info(MarkupHelper.createLabel(log, ExtentColor.GREY));
+    }
+    public static void logWarnDetails(String log){
+        Setup.extentTest.get().warning(MarkupHelper.createLabel(log, ExtentColor.YELLOW));
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/reporting/Setup.java
+++ src/main/java/reporting/Setup.java
@@ -1,12 +1,16 @@
 package reporting;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
 
 public class Setup implements ITestListener {
 
     private static ExtentReports extentReports;
+    public static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
+
 
 //  Initializing the Report
     @Override
@@ -21,5 +25,12 @@ public class Setup implements ITestListener {
     public void onFinish(ITestContext context) {
         if (extentReports != null)
                 extentReports.flush();
+    }
+//  Attaching tests to the report
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest test = extentReports.createTest("Test Name "+result.getTestClass().getName()+" - "+result.getMethod().getMethodName());
+        extentTest.set(test);
     }
 }
